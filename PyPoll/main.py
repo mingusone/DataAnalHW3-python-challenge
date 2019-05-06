d96a050ca88b572db04f9ede81419fdfc8abca9b
minglei2161@gmail.com <6264548+mingusone@users.noreply.github.com>
2019-05-06 17:18:22 -0400

    Main for loop completed. Candidates dict now works. Shows candidate name and the number of votes they got

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,41 @@
+
+
 import os
 import csv
 
-csvPath = os.path.join("Resource", "election_data.csv")
+csvPath = os.path.join("Resources", "election_data.csv")
+
+# The total number of votes cast
+totalVotes = 0
+
+# A complete list of candidates who received votes
+candidates = {}
+
+# The percentage of votes each candidate won
+#for candidate in candidates...
+
+# The total number of votes each candidate won
+#look at candidates dict
+
+# The winner of the election based on popular vote.
 
 with open(csvPath,newline="") as csvFile:
     csvReader = csv.reader(csvFile, delimiter=",")
+    next(csvReader) #header stuff
+    for row in csvReader:
+        candidate = row[2]
+        totalVotes += 1
+        try:
+            candidates[candidate]
+        except:
+            candidates[candidate] = 0
+        
+        candidates[candidate] = candidates[candidate] + 1
+
+    print(candidates)
+
+        # try:
+        #     pieShoppingBag[pieSelection[pieChoice-1]]
+        #     print("we got an error! But we really shouldn't since we mapped it out up top :X")
+        # except:
+        #     pieShoppingBag[pieSelection[pieChoice-1]] = 0
